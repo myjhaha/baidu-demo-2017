d402dae18828a4a1f793abee2e3311cdae7aa111
myjhaha <cs_terrorist@126.com>
2017-03-13 01:53:12 +0800

    vue-learning (2)

--- baidu_nuomi/02-data-binding-01/vue-myj-v.0.1.js
+++ baidu_nuomi/02-data-binding-01/vue-myj-v.0.1.js
@@ -1,5 +1,7 @@
+
 function Observer(data){
 	this.data = data;
+	//def(data, '__ob__', this);	//给一个指针指回去
 	this.walk(data);
 }
 
@@ -7,21 +9,21 @@ let p = Observer.prototype;
 
 p.walk = function(obj){
 	let val; 
+	let _this = this;
 	for (let key in obj) {
 		if (obj.hasOwnProperty(key)){
 			val = obj[key];
-
-			if (typeof val === 'object'){
+			_this.makeSetterAndgetter(key, val);
+			if (typeof val === 'object') {
 				new Observer(val);
 			}
-
-			this.convert(key, val);
 		}
 	}
 }
 
-p.convert = function (key, val){
-	Object.defineProperty(this.data, key, {
+p.makeSetterAndgetter = function (key, val){
+	let _this = this;
+	Object.defineProperty(_this.data, key, {
 		configurable:true,
 		enumerable: true,
 		get :function() {
